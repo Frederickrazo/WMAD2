efeaf159b724558e38bf7d8df879091304aabb0f
Frederickrazo <130958638+Frederickrazo@users.noreply.github.com>
2023-04-24 13:46:45 +0800

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const answerForm = document.getElementById('answer-form');
+const answerInput = document.getElementById('answer');
+const resultElement = document.getElementById('result');
+
+answerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const answer = Number(answerInput.value);
+  if (answer === 12) {
+    resultElement.innerHTML = 'Correct!';
+    resultElement.style.color = 'green';
+  } else {
+    resultElement.innerHTML = 'Incorrect. The answer is 12.';
+    resultElement.style.color = 'red';
+  }
+});
